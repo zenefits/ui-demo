d9c890b3fd2d67609283f3b76c57169b13e84c27
Eric Lafleur <elafleur@coveo.com>
2019-05-01 13:27:32 -0400

    add submit and cancel button handlers

--- components/forms/src/CoveoDeflectionSearch.tsx
+++ components/forms/src/CoveoDeflectionSearch.tsx
@@ -12,10 +12,14 @@ type CoveoDeflectionSearchProps = {
 class SearchUI extends Component<CoveoDeflectionSearchProps> {
   searchInterface: React.RefObject<HTMLDivElement>;
   searchTimeout: number;
+  shouldLogOnUnload: boolean;
 
   constructor(props: CoveoDeflectionSearchProps) {
     super(props);
     this.searchInterface = React.createRef();
+    this.shouldLogOnUnload = true;
+    this.onUnload = this.onUnload.bind(this);
+    this.handleBuildingQuery = this.handleBuildingQuery.bind(this);
   }
 
   handleBuildingQuery(e: Event, args: Coveo.IBuildingQueryEventArgs) {
@@ -27,9 +31,40 @@ class SearchUI extends Component<CoveoDeflectionSearchProps> {
     }
   }
 
+  logCancelButtonEvent() {
+    Coveo.logCustomEvent(this.searchInterface.current, {
+      name: 'cancelButton',
+      type: 'caseCreation'
+    }, {});
+
+    // Don't log an unload page event since we cancelled the case
+    this.shouldLogOnUnload = false;
+  }
+
+  logSubmitButtonEvent() {    
+    Coveo.logCustomEvent(this.searchInterface.current, {
+      name: 'submitButton',
+      type: 'caseCreation'
+    }, {});
+
+    // Don't log an unload page event since we submitted the case
+    this.shouldLogOnUnload = false;
+  }
+
+  onUnload() {
+    if (this.shouldLogOnUnload) {
+      Coveo.logCustomEvent(this.searchInterface.current, {
+        name: 'unloadPage',
+        type: 'caseCreation'
+      }, {});
+    }
+  }
+
   componentDidMount() {
+    window.addEventListener('beforeunload', this.onUnload);
+
     Coveo.SearchEndpoint.configureCloudV2Endpoint(this.props.organizationId, this.props.accessToken);
-    Coveo.$$(this.searchInterface.current).on('buildingQuery', this.handleBuildingQuery.bind(this));
+    Coveo.$$(this.searchInterface.current).on('buildingQuery', this.handleBuildingQuery);
     Coveo.init(this.searchInterface.current);
   }
 
@@ -37,9 +72,19 @@ class SearchUI extends Component<CoveoDeflectionSearchProps> {
     clearTimeout(this.searchTimeout);
 
     this.searchTimeout = window.setTimeout(() => {
-      Coveo.logSearchEvent(this.searchInterface.current, { name: 'inputChange', type: 'caseCreation' }, {});
+      Coveo.logSearchEvent(this.searchInterface.current, {
+        name: 'inputChange',
+        type: 'caseCreation'
+      }, {});
       Coveo.executeQuery(this.searchInterface.current);
     }, this.props.delayMilliseconds || 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.searchTimeout);
+
+    window.removeEventListener('beforeunload', this.onUnload);
+    Coveo.$$(this.searchInterface.current).off('buildingQuery', this.handleBuildingQuery);
   }
 
   render() {
@@ -52,6 +97,15 @@ class SearchUI extends Component<CoveoDeflectionSearchProps> {
 }
 
 class CoveoDeflection extends Component<CoveoDeflectionSearchProps> {
+  child: React.RefObject<SearchUI>;
+
+  constructor(props: CoveoDeflectionSearchProps) {
+    super(props);
+    this.child = React.createRef();
+    this.triggerCancelButton = this.triggerCancelButton.bind(this);
+    this.triggerSubmitButton = this.triggerSubmitButton.bind(this);
+  }
+  
   componentDidMount() {
     Coveo.TemplateCache.registerTemplate("default", Coveo.HtmlTemplate.fromString(`
       <div class='coveo-result-frame'>
@@ -71,10 +125,18 @@ class CoveoDeflection extends Component<CoveoDeflectionSearchProps> {
       }), true, true);
   }
 
+  triggerCancelButton(){
+    this.child.current.logCancelButtonEvent();
+  }
+
+  triggerSubmitButton(){
+    this.child.current.logSubmitButtonEvent();
+  }
+
   render() {
     return (
       <div>
-        <SearchUI {...this.props}>
+        <SearchUI {...this.props} ref={this.child}>
           <div className="CoveoAnalytics" />
           <div className="coveo-search-section" />
           <div className="coveo-main-section">
